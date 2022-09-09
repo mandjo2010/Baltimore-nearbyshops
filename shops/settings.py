c0d2e79e9254decbf81faa51c87b1ff571dd9b03
mandjo2010 <boremandjo@gmail.com>
2022-09-10 01:29:22 +0200

    integrating the map using leaflet to the app

--- shops/settings.py
+++ shops/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -47,7 +48,7 @@ ROOT_URLCONF = 'shops.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
